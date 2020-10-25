6b02d6ab8f61856c1ae0355b76581476b6aad7f6
Siddhant Jain <siddhant.jain120@gmail.com>
2020-10-25 21:44:40 +0530

    TASK 1 no errors

--- trap.c
+++ trap.c
@@ -37,9 +37,8 @@ void
 trap(struct trapframe *tf)
 {
   if(tf->trapno == T_SYSCALL){
-    if(myproc()->killed){
+    if(myproc()->killed)
       exit();
-    }
     myproc()->tf = tf;
     syscall();
     if(myproc()->killed)
@@ -52,7 +51,6 @@ trap(struct trapframe *tf)
     if(cpuid() == 0){
       acquire(&tickslock);
       ticks++;
-      myproc()->rtime++;
       wakeup(&ticks);
       release(&tickslock);
     }
